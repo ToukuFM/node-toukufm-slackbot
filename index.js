04e50c43721db8c9a30efe6c684ee1ca5ad2a213
Bakaboykie <bakaboykie@gmail.com>
2016-01-07 19:27:58 +0000

    Initial bot release

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-// Requires ================================================================== #
-var mongoose = require('mongoose');
-var slackAPI = require('slackbotapi');
-var slackEventHandler = require('./scripts/slack-events');
-
 // Setup ===================================================================== #
-Date.prototype.getWeek = require('./scripts/get-week');
+var mongoose = require('mongoose');
+var slack = require('./lib/slack'); // Run the slackbot
 
 // Connect to DB
 mongoose.connect('mongodb://localhost/toukufm');
 
-// Connect to Slack
-var slack = new slackAPI({
-    'token': 'TOKENHERE',
-    'logging': true,
-    'autoReconnect': true
-});
-
-var events = new slackEventHandler(slack);
+// slack.doSomething("ayy");
